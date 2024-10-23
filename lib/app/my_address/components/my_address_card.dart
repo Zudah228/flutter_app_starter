203c3c8f42b362b852cfd2f2db5ccdf4e1db5e2d
Zudah228 <rezel95.hin@gmail.com>
2024-10-23 23:41:52 +0900

    feat: 住所詳細ページの作成

--- lib/app/my_address/components/my_address_card.dart
+++ lib/app/my_address/components/my_address_card.dart
@@ -7,6 +7,7 @@ import '../../../app_core/snack_bar.dart';
 import '../../../domain/my_address/my_address.dart';
 import '../../../domain/my_address/my_address_repository.dart';
 import '../../../util/extensions/string_extension.dart';
+import '../my_address_details_page.dart';
 import '../my_address_edit_page.dart';
 
 class MyAddressCard extends ConsumerWidget {
@@ -16,12 +17,9 @@ class MyAddressCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeData = Theme.of(context);
-    final colorScheme = themeData.colorScheme;
-
     Future<void> onTap() async {
       Navigator.of(context).push(
-        MyAddressEditPage.route((initialValue: address)),
+        MyAddressDetailsPage.route((address: address)),
       );
     }
 
@@ -41,7 +39,7 @@ class MyAddressCard extends ConsumerWidget {
 
               // 説明
               if (address.description?.nullIfEmpty case final description?) ...[
-                const SizedBox(height: 12),
+                const SizedBox(height: 16),
                 Row(
                   children: [
                     const Icon(
@@ -52,34 +50,6 @@ class MyAddressCard extends ConsumerWidget {
                     Text(description),
                   ],
                 )
-              ],
-
-              // URL
-              if (address.url?.nullIfEmpty case final url?) ...[
-                const SizedBox(height: 12),
-                Material(
-                  type: MaterialType.transparency,
-                  textStyle: TextStyle(
-                    color: colorScheme.onPrimaryContainer,
-                  ),
-                  child: InkWell(
-                    onTap: () async {
-                      await Clipboard.setData(ClipboardData(text: url));
-
-                      showSnackBar(message: 'コピーしました');
-                    },
-                    child: const Row(
-                      children: [
-                        Icon(
-                          Icons.share,
-                          size: 16,
-                        ),
-                        SizedBox(width: 4),
-                        Text('位置情報をコピー')
-                      ],
-                    ),
-                  ),
-                ),
               ],
             ],
           ),
@@ -115,6 +85,18 @@ class _Title extends ConsumerWidget {
       } catch (_) {}
     }
 
+    Future<void> edit() async {
+      Navigator.of(context).push(
+        MyAddressEditPage.route((initialValue: address)),
+      );
+    }
+
+    Future<void> copyLink(String url) async {
+      await Clipboard.setData(ClipboardData(text: url));
+
+      showSnackBar(message: 'コピーしました');
+    }
+
     return Row(
       children: [
         Expanded(
@@ -142,11 +124,34 @@ class _Title extends ConsumerWidget {
                 child: const Row(
                   children: [
                     Icon(Icons.delete),
-                    SizedBox(width: 4),
+                    SizedBox(width: 6),
                     Text('削除')
                   ],
                 ),
-              )
+              ),
+              PopupMenuItem(
+                onTap: edit,
+                child: const Row(
+                  children: [
+                    Icon(Icons.edit),
+                    SizedBox(width: 6),
+                    Text('編集'),
+                  ],
+                ),
+              ),
+              if (address.url?.nullIfEmpty case final url?)
+                PopupMenuItem(
+                  onTap: () {
+                    copyLink(url);
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.share_location),
+                      SizedBox(width: 6),
+                      Text('URL をコピー')
+                    ],
+                  ),
+                ),
             ];
           },
         ),
---
+++ lib/app/my_address/my_address_details_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../../app_core/snack_bar.dart';
+import '../../domain/my_address/my_address.dart';
+import '../../util/extensions/string_extension.dart';
+
+typedef MyAddressDetailsPageArguments = _PageArguments;
+typedef _PageArguments = ({MyAddress address});
+
+class MyAddressDetailsPage extends StatefulWidget {
+  const MyAddressDetailsPage._(this.arguments);
+
+  static const routeName = '/my_address_details';
+
+  static Route<void> route(MyAddressDetailsPageArguments arguments) {
+    return MaterialPageRoute(
+      settings: RouteSettings(name: routeName, arguments: arguments),
+      builder: (_) => MyAddressDetailsPage._(arguments),
+    );
+  }
+
+  final MyAddressDetailsPageArguments arguments;
+
+  @override
+  State<MyAddressDetailsPage> createState() => _MyAddressDetailsPageState();
+}
+
+class _MyAddressDetailsPageState extends State<MyAddressDetailsPage> {
+  MyAddress get address => widget.arguments.address;
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final textTheme = themeData.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('住所詳細'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 16),
+              _ClipboardText(
+                address.prefecture +
+                    address.city +
+                    address.address1 +
+                    address.address2,
+                caption: '長押しで全文をコピー',
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      address.prefecture,
+                      style: textTheme.headlineSmall,
+                    ),
+                    Text(address.city + address.address1 + address.address2),
+                  ],
+                ),
+              ),
+
+              // URL
+              if (address.url?.nullIfEmpty case final url?) ...[
+                const SizedBox(height: 16),
+                _ClipboardText(url),
+              ],
+
+              // 説明
+              if (address.description?.nullIfEmpty case final description?) ...[
+                const SizedBox(height: 16),
+                _ClipboardText(description),
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ClipboardText extends StatelessWidget {
+  const _ClipboardText(this.text, {this.caption = '長押しでコピー', this.child});
+
+  final String text;
+  final Widget? child;
+  final String caption;
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final textTheme = themeData.textTheme;
+    final colorScheme = themeData.colorScheme;
+
+    return Material(
+      type: MaterialType.transparency,
+      child: InkWell(
+        onLongPress: () async {
+          await Clipboard.setData(ClipboardData(text: text));
+
+          showSnackBar(message: 'コピーしました');
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            child ?? Text(text),
+            const SizedBox(height: 4),
+            Text(
+              caption,
+              style: textTheme.bodySmall!.copyWith(
+                color: colorScheme.onSurfaceVariant,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
