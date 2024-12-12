fa361a4a6e168197a6d069394df7bfb2cb5a0eed
Zudah228 <rezel95.hin@gmail.com>
2024-12-12 11:20:01 +0900

    コメント追加

--- lib/util/extensions/string_extension.dart
+++ lib/util/extensions/string_extension.dart
@@ -1,3 +1,12 @@
 extension StringExtension on String {
+  /// isEmpty なら Null で返す
+  ///
+  /// ```
+  /// [
+  ///   if (object.description.nullIfEmpty final case description?) {
+  ///     Text(description),
+  ///   }
+  /// ]
+  /// ```
   String? get nullIfEmpty => isEmpty ? null : this;
 }
