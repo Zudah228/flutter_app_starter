fffe727e0c3de1a278ced9cb1fa41c2c3ee65ea9
Zudah228 <rezel95.hin@gmail.com>
2024-12-12 11:29:57 +0900

    packageInfoService の初期化

--- lib/dependency_injection/dependency_injection.dart
+++ lib/dependency_injection/dependency_injection.dart
@@ -1,19 +1,26 @@
 import 'package:riverpod/riverpod.dart' show Override;
 
+import '../service/package_info/package_info.dart';
 import '../service/shared_preferences/shared_preferences.dart';
 
 abstract final class DependencyInjection {
   static Future<List<Override>> overrides() async {
     late final SharedPreferencesService sharedPreferencesService;
+    late final PackageInfoService packageInfoService;
 
     await Future.wait([
       Future(() async {
         sharedPreferencesService = await SharedPreferencesService.init();
       }),
+      Future(() async {
+        packageInfoService = await PackageInfoService.init();
+      }),
     ]);
 
     return [
-      sharedPreferencesServiceProvider.overrideWithValue(sharedPreferencesService)
+      sharedPreferencesServiceProvider
+          .overrideWithValue(sharedPreferencesService),
+      packageInfoServiceProvider.overrideWithValue(packageInfoService),
     ];
   }
 }
